e43f96c1e6a0b7e7120e22c9952448662cd5fa18
dulguuncodes <81787757+dulguuncodes@users.noreply.github.com>
2021-07-18 20:05:39 +0800

    fix highlighting issue with gitgutter

--- lua/highlight.lua
+++ lua/highlight.lua
@@ -9,8 +9,8 @@ end
 hi({'BufferVisible', 'BufferVisibleSign', 'BufferVisibleMod'}, '#e2e2e3', '#2F383E')
 hi({'BufferInactive', 'BufferInactiveSign'}, '#434851', '#2F383E')
 hi({'SignColumn'}, '#d3c6aa', '#2B3339')
-hi({'CocInfoSign', 'GitGutterAdd', 'GitSignsAdd'}, '#2b3339', '#6fa19b')
-hi({'CocErrorSign', 'GitGutterDelete', 'GitSignsDelete'}, '#2b3339', '#e17d8f')
-hi({'GitGutterChange', 'GitSignsChange'}, '#2b3339', '#a3be8c')
+hi({'CocInfoSign', 'GitGutterAdd', 'GitSignsAdd'}, '#6fa19b', '#2b3339')
+hi({'CocErrorSign', 'GitGutterDelete', 'GitSignsDelete'},'#e17d8f', '#2b3339')
+hi({'GitGutterChange', 'GitSignsChange'}, '#a3be8c', '#2b3339')
 hi({'Statusline', 'StatuslineNC'}, '#2B3339', '#2B3339')
 
