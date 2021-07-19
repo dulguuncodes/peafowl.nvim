8acbce28b9af214790bdb67620bc5c12fbf1356d
dulguuncodes <81787757+dulguuncodes@users.noreply.github.com>
2021-07-19 20:48:08 +0800

    add ensure_installed in nvim-treesitter config

--- lua/treesitter.lua
+++ lua/treesitter.lua
@@ -1,7 +1,8 @@
 require("nvim-treesitter.configs").setup {
+	  ensure_installed = {"bash", "dart", "dockerfile", "elixir", "fish", "go", "graphql", "html", "javascript", "json", "lua", "python", "tsx", "typescript", "yaml"},
     highlight = {
         enable = true,
-        disable = {"tsx"}
+        disable = {"tsx"} -- typescript react files somehow look better without nvim-treesitter
     },
     indent = {
         enable = true
