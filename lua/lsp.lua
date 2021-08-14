962022c9593fa691985d6c8138a73530d5f84356
dulguuncodes <81787757+dulguuncodes@users.noreply.github.com>
2021-08-14 19:21:17 +0800

    feat: use luafmt for formatting files and format some files
    yay

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,68 +1,72 @@
-local lspconfig = require('lspconfig')
+local lspconfig = require("lspconfig")
 local g = vim.g
 
 local on_attach = function(client, bufnr)
-    local buf_map = vim.api.nvim_buf_set_keymap
-    vim.cmd("command! LspDef lua vim.lsp.buf.definition()")
-    vim.cmd("command! LspFormatting lua vim.lsp.buf.formatting()")
-    vim.cmd("command! LspCodeAction lua vim.lsp.buf.code_action()")
-    vim.cmd("command! LspHover lua vim.lsp.buf.hover()")
-    vim.cmd("command! LspRename lua vim.lsp.buf.rename()")
-    vim.cmd("command! LspOrganize lua lsp_organize_imports()")
-    vim.cmd("command! LspRefs lua vim.lsp.buf.references()")
-    vim.cmd("command! LspTypeDef lua vim.lsp.buf.type_definition()")
-    vim.cmd("command! LspImplementation lua vim.lsp.buf.implementation()")
-    vim.cmd("command! LspDiagPrev lua vim.lsp.diagnostic.goto_prev()")
-    vim.cmd("command! LspDiagNext lua vim.lsp.diagnostic.goto_next()")
-    vim.cmd("command! LspDiagLine lua vim.lsp.diagnostic.show_line_diagnostics()")
-    vim.cmd("command! LspSignatureHelp lua vim.lsp.buf.signature_help()")buf_map(bufnr, "n", "gd", ":LspDef<CR>", {silent = true})
+  local buf_map = vim.api.nvim_buf_set_keymap
+  vim.cmd("command! LspDef lua vim.lsp.buf.definition()")
+  vim.cmd("command! LspFormatting lua vim.lsp.buf.formatting()")
+  vim.cmd("command! LspCodeAction lua vim.lsp.buf.code_action()")
+  vim.cmd("command! LspHover lua vim.lsp.buf.hover()")
+  vim.cmd("command! LspRename lua vim.lsp.buf.rename()")
+  vim.cmd("command! LspOrganize lua lsp_organize_imports()")
+  vim.cmd("command! LspRefs lua vim.lsp.buf.references()")
+  vim.cmd("command! LspTypeDef lua vim.lsp.buf.type_definition()")
+  vim.cmd("command! LspImplementation lua vim.lsp.buf.implementation()")
+  vim.cmd("command! LspDiagPrev lua vim.lsp.diagnostic.goto_prev()")
+  vim.cmd("command! LspDiagNext lua vim.lsp.diagnostic.goto_next()")
+  vim.cmd("command! LspDiagLine lua vim.lsp.diagnostic.show_line_diagnostics()")
+  vim.cmd("command! LspSignatureHelp lua vim.lsp.buf.signature_help()")
+  buf_map(bufnr, "n", "gd", ":LspDef<CR>", {silent = true})
 
-    buf_map(bufnr, "n", "gr", ":LspRename<CR>", {silent = true})
-    buf_map(bufnr, "n", "gR", ":LspRefs<CR>", {silent = true})
-    buf_map(bufnr, "n", "gy", ":LspTypeDef<CR>", {silent = true})
-    buf_map(bufnr, "n", "K", ":LspHover<CR>", {silent = true})
-    buf_map(bufnr, "n", "gs", ":LspOrganize<CR>", {silent = true})
-    buf_map(bufnr, "n", "[a", ":LspDiagPrev<CR>", {silent = true})
-    buf_map(bufnr, "n", "]a", ":LspDiagNext<CR>", {silent = true})
-    buf_map(bufnr, "n", "ga", ":LspCodeAction<CR>", {silent = true})
-    buf_map(bufnr, "n", "<Leader>a", ":LspDiagLine<CR>", {silent = true})
-    buf_map(bufnr, "i", "<C-x><C-x>", "<cmd> LspSignatureHelp<CR>", {silent = true})
-    if client.resolved_capabilities.document_formatting then
-        vim.api.nvim_exec([[
+  buf_map(bufnr, "n", "gr", ":LspRename<CR>", {silent = true})
+  buf_map(bufnr, "n", "gR", ":LspRefs<CR>", {silent = true})
+  buf_map(bufnr, "n", "gy", ":LspTypeDef<CR>", {silent = true})
+  buf_map(bufnr, "n", "K", ":LspHover<CR>", {silent = true})
+  buf_map(bufnr, "n", "gs", ":LspOrganize<CR>", {silent = true})
+  buf_map(bufnr, "n", "[a", ":LspDiagPrev<CR>", {silent = true})
+  buf_map(bufnr, "n", "]a", ":LspDiagNext<CR>", {silent = true})
+  buf_map(bufnr, "n", "ga", ":LspCodeAction<CR>", {silent = true})
+  buf_map(bufnr, "n", "<Leader>a", ":LspDiagLine<CR>", {silent = true})
+  buf_map(bufnr, "i", "<C-x><C-x>", "<cmd> LspSignatureHelp<CR>", {silent = true})
+  if client.resolved_capabilities.document_formatting then
+    vim.api.nvim_exec(
+      [[
          augroup LspAutocommands
              autocmd! * <buffer>
              autocmd BufWritePost <buffer> LspFormatting
          augroup END
-         ]], true)
-    end
+         ]],
+      true
+    )
+  end
 end
-
 
 local servers = {
   sumneko_lua = {
-    cmd = { g.peafowl_lua_lsp_location, '-E', g.peafowl_lua_lsp_lib },
+    cmd = {g.peafowl_lua_lsp_location, "-E", g.peafowl_lua_lsp_lib},
     diagnostics = {
-      globals = { 'vim' }
+      globals = {"vim"}
     },
     workspace = {
-      library = vim.api.nvim_get_runtime_file('', true),
+      library = vim.api.nvim_get_runtime_file("", true)
     },
     telemetry = {
-      enable = false,
-    },
+      enable = false
+    }
   },
   elixirls = {
-    cmd = { g.peafowl_elixir_lsp_location }
+    cmd = {g.peafowl_elixir_lsp_location}
   },
   diagnosticls = {
-    filetypes = {"javascript", "javascriptreact", "typescript", "typescriptreact", "css", "html", "json"},
+    filetypes = {"javascript", "javascriptreact", "typescript", "typescriptreact", "css", "html", "json", "lua"},
     init_options = {
       filetypes = {
         javascript = "eslint",
         typescript = "eslint",
         javascriptreact = "eslint",
         typescriptreact = "eslint",
-        json = "prettier"
+        json = "prettier",
+        lua = "luafmt"
       },
       linters = {
         eslint = {
@@ -98,21 +102,30 @@ local servers = {
       },
       formatters = {
         prettier = {
-          command = 'prettier',
-          args = { '--stdin-filepath', '%filename' }
+          command = "prettier",
+          args = {"--stdin-filepath", "%filename"}
+        },
+        luafmt = {
+          command = "luafmt",
+          args = {
+            "--indent-count",
+            "2",
+            "--stdin"
+          }
         }
       },
       formatFiletypes = {
-        css = 'prettier',
-        javascript = 'prettier',
-        javascriptreact = 'prettier',
-        json = 'prettier',
-        scss = 'prettier',
-        less = 'prettier',
-        typescript = 'prettier',
-        typescriptreact = 'prettier',
-        markdown = 'prettier',
-      },
+        css = "prettier",
+        javascript = "prettier",
+        javascriptreact = "prettier",
+        json = "prettier",
+        scss = "prettier",
+        less = "prettier",
+        typescript = "prettier",
+        typescriptreact = "prettier",
+        markdown = "prettier",
+        lua = "luafmt"
+      }
     }
   },
   tsserver = {
@@ -120,7 +133,7 @@ local servers = {
       client.resolved_capabilities.document_formatting = false
       on_attach(client)
     end,
-    cmd = { "/opt/homebrew/bin/typescript-language-server", "--stdio" }
+    cmd = {"/opt/homebrew/bin/typescript-language-server", "--stdio"}
   }
 }
 
@@ -144,4 +157,3 @@ for lsp_name, lsp_config in pairs(servers) do
 
   ::continue::
 end
-
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -3,129 +3,122 @@ local use = packer.use
 
 vim.cmd [[packadd dashboard-nvim]]
 
-return require("packer").startup(function()
-    use 'wbthomason/packer.nvim'
+return require("packer").startup(
+  function()
+    use "wbthomason/packer.nvim"
 
     use {
-      'sainnhe/everforest',
+      "sainnhe/everforest",
       config = function()
         vim.cmd("colorscheme everforest")
         require("highlight")
       end
     }
 
-    use {'hrsh7th/nvim-compe', config = function()
-        require'compe'.setup {
-          enabled = true;
-      autocomplete = true;
-    debug = false;
-    min_length = 1;
-    preselect = 'enable';
-    throttle_time = 80;
-    source_timeout = 200;
-    resolve_timeout = 800;
-    incomplete_delay = 400;
-    max_abbr_width = 100;
-    max_kind_width = 100;
-    max_menu_width = 100;
-    documentation = {
-      border = { '', '' ,'', ' ', '', '', '', ' ' }, -- the border option is the same as `|help nvim_open_win|`
-      winhighlight = "NormalFloat:CompeDocumentation,FloatBorder:CompeDocumentationBorder",
-      max_width = 120,
-      min_width = 60,
-      max_height = math.floor(vim.o.lines * 0.3),
-      min_height = 1,
-    };
-
-    source = {
-      path = true;
-      buffer = true;
-      calc = true;
-      nvim_lsp = true;
-      nvim_lua = true;
-      vsnip = true;
-      ultisnips = true;
-      luasnip = true;
-    };
-  }
-    end}
-
     use {
-        'glepnir/galaxyline.nvim',
-        branch = 'main',
-        requires = {
-            'kyazdani42/nvim-web-devicons',
-            opt = true
+      "hrsh7th/nvim-compe",
+      config = function()
+        require "compe".setup {
+          enabled = true,
+          autocomplete = true,
+          debug = false,
+          min_length = 1,
+          preselect = "enable",
+          throttle_time = 80,
+          source_timeout = 200,
+          resolve_timeout = 800,
+          incomplete_delay = 400,
+          max_abbr_width = 100,
+          max_kind_width = 100,
+          max_menu_width = 100,
+          documentation = {
+            border = {"", "", "", " ", "", "", "", " "}, -- the border option is the same as `|help nvim_open_win|`
+            winhighlight = "NormalFloat:CompeDocumentation,FloatBorder:CompeDocumentationBorder",
+            max_width = 120,
+            min_width = 60,
+            max_height = math.floor(vim.o.lines * 0.3),
+            min_height = 1
+          },
+          source = {
+            path = true,
+            buffer = true,
+            calc = true,
+            nvim_lsp = true,
+            nvim_lua = true,
+            vsnip = true,
+            ultisnips = true,
+            luasnip = true
+          }
         }
+      end
     }
 
     use {
-        'kyazdani42/nvim-tree.lua',
-        cmd = "NvimTreeToggle"
+      "glepnir/galaxyline.nvim",
+      branch = "main",
+      requires = {
+        "kyazdani42/nvim-web-devicons",
+        opt = true
+      }
     }
 
     use {
-        'nvim-treesitter/nvim-treesitter',
-        run = ":TSUpdate",
-        config = function()
-            require("treesitter")
-        end
-    }
---     use {
---         'neoclide/coc.nvim',
---         branch = 'release'
---     }
-
-    use 'romgrk/barbar.nvim'
-    use 'tpope/vim-obsession'
-
-    use 'wesQ3/vim-windowswap'
-    use 'lukas-reineke/indent-blankline.nvim'
-    use {
-        'RRethy/vim-hexokinase',
-        run = 'make hexokinase'
+      "kyazdani42/nvim-tree.lua",
+      cmd = "NvimTreeToggle"
     }
 
     use {
-        'lewis6991/gitsigns.nvim',
-        requires = {'nvim-lua/plenary.nvim'},
-        config = function()
-            require('gitsigns').setup()
-        end
+      "nvim-treesitter/nvim-treesitter",
+      run = ":TSUpdate",
+      config = function()
+        require("treesitter")
+      end
     }
 
-    use 'airblade/vim-gitgutter'
-    use 'tpope/vim-fugitive'
+    use "romgrk/barbar.nvim"
+    use "tpope/vim-obsession"
 
-    use 'jiangmiao/auto-pairs'
-    use 'machakann/vim-highlightedyank'
-
+    use "wesQ3/vim-windowswap"
+    use "lukas-reineke/indent-blankline.nvim"
     use {
-        'dulguuncodes/dashboard-nvim',
-        cmd = {"Dashboard", "DashboardNewFile", "DashboardJumpMarks", "SessionLoad", "SessionSave"},
-        setup = function()
-            require("dashboard")
-        end
+      "RRethy/vim-hexokinase",
+      run = "make hexokinase"
     }
 
     use {
-        'nvim-telescope/telescope.nvim',
-        requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}
+      "lewis6991/gitsigns.nvim",
+      requires = {"nvim-lua/plenary.nvim"},
+      config = function()
+        require("gitsigns").setup()
+      end
+    }
+
+    use "airblade/vim-gitgutter"
+    use "tpope/vim-fugitive"
+
+    use "jiangmiao/auto-pairs"
+    use "machakann/vim-highlightedyank"
+
+    use {
+      "dulguuncodes/dashboard-nvim",
+      cmd = {"Dashboard", "DashboardNewFile", "DashboardJumpMarks", "SessionLoad", "SessionSave"},
+      setup = function()
+        require("dashboard")
+      end
     }
 
     use {
-        'terrortylor/nvim-comment',
-        config = function()
-            require("comment")
-        end
+      "nvim-telescope/telescope.nvim",
+      requires = {{"nvim-lua/popup.nvim"}, {"nvim-lua/plenary.nvim"}}
     }
-
-    use 'neovim/nvim-lspconfig'
 
     use {
-      'iamcco/markdown-preview.nvim',
-      run = 'cd app && yarn install',
-      cmd = 'MarkdownPreview'
+      "terrortylor/nvim-comment",
+      config = function()
+        require("comment")
+      end
     }
-end)
 
+    use "neovim/nvim-lspconfig"
+  end
+)
