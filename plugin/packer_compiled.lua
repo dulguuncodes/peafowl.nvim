4242c57d97424710893027d03ecd3c8f2aeb1a7a
dulguuncodes <81787757+dulguuncodes@users.noreply.github.com>
2022-01-21 13:28:45 +0800

    yes

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -57,7 +57,7 @@ end
 time([[Luarocks path setup]], false)
 time([[try_loadstring definition]], true)
 local function try_loadstring(s, component, name)
-  local success, result = pcall(loadstring(s))
+  local success, result = pcall(loadstring(s), name, _G.packer_plugins[name])
   if not success then
     vim.schedule(function()
       vim.api.nvim_notify('packer.nvim: Error running ' .. component .. ' for ' .. name .. ': ' .. result, vim.log.levels.ERROR, {})
@@ -71,107 +71,153 @@ time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
   ["auto-pairs"] = {
     loaded = true,
-    path = "/Users/dulguuncodes/.local/share/nvim/site/pack/packer/start/auto-pairs"
+    path = "/Users/dulguuncodes/.local/share/nvim/site/pack/packer/start/auto-pairs",
+    url = "https://github.com/jiangmiao/auto-pairs"
   },
   ["barbar.nvim"] = {
     loaded = true,
-    path = "/Users/dulguuncodes/.local/share/nvim/site/pack/packer/start/barbar.nvim"
+    path = "/Users/dulguuncodes/.local/share/nvim/site/pack/packer/start/barbar.nvim",
+    url = "https://github.com/romgrk/barbar.nvim"
   },
   ["dashboard-nvim"] = {
     loaded = true,
     needs_bufread = false,
-    path = "/Users/dulguuncodes/.local/share/nvim/site/pack/packer/opt/dashboard-nvim"
+    path = "/Users/dulguuncodes/.local/share/nvim/site/pack/packer/opt/dashboard-nvim",
+    url = "https://github.com/dulguuncodes/dashboard-nvim"
   },
   everforest = {
     config = { "\27LJ\2\nX\0\0\3\0\5\0\b6\0\0\0009\0\1\0'\2\2\0B\0\2\0016\0\3\0'\2\4\0B\0\2\1K\0\1\0\14highlight\frequire\27colorscheme everforest\bcmd\bvim\0" },
     loaded = true,
-    path = "/Users/dulguuncodes/.local/share/nvim/site/pack/packer/start/everforest"
+    path = "/Users/dulguuncodes/.local/share/nvim/site/pack/packer/start/everforest",
+    url = "https://github.com/sainnhe/everforest"
   },
   ["galaxyline.nvim"] = {
     loaded = true,
-    path = "/Users/dulguuncodes/.local/share/nvim/site/pack/packer/start/galaxyline.nvim"
+    path = "/Users/dulguuncodes/.local/share/nvim/site/pack/packer/start/galaxyline.nvim",
+    url = "https://github.com/glepnir/galaxyline.nvim"
   },
   ["gitsigns.nvim"] = {
     config = { "\27LJ\2\n6\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\rgitsigns\frequire\0" },
     loaded = true,
-    path = "/Users/dulguuncodes/.local/share/nvim/site/pack/packer/start/gitsigns.nvim"
+    path = "/Users/dulguuncodes/.local/share/nvim/site/pack/packer/start/gitsigns.nvim",
+    url = "https://github.com/lewis6991/gitsigns.nvim"
   },
   ["indent-blankline.nvim"] = {
     loaded = true,
-    path = "/Users/dulguuncodes/.local/share/nvim/site/pack/packer/start/indent-blankline.nvim"
+    path = "/Users/dulguuncodes/.local/share/nvim/site/pack/packer/start/indent-blankline.nvim",
+    url = "https://github.com/lukas-reineke/indent-blankline.nvim"
   },
   ["nord.nvim"] = {
     loaded = true,
-    path = "/Users/dulguuncodes/.local/share/nvim/site/pack/packer/start/nord.nvim"
+    path = "/Users/dulguuncodes/.local/share/nvim/site/pack/packer/start/nord.nvim",
+    url = "https://github.com/shaunsingh/nord.nvim"
   },
   ["nvim-cartographer"] = {
     config = { "\27LJ\2\n$\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\tmaps\frequire\0" },
     loaded = true,
-    path = "/Users/dulguuncodes/.local/share/nvim/site/pack/packer/start/nvim-cartographer"
+    path = "/Users/dulguuncodes/.local/share/nvim/site/pack/packer/start/nvim-cartographer",
+    url = "https://github.com/Iron-E/nvim-cartographer"
   },
   ["nvim-comment"] = {
     config = { "\27LJ\2\n'\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\fcomment\frequire\0" },
     loaded = true,
-    path = "/Users/dulguuncodes/.local/share/nvim/site/pack/packer/start/nvim-comment"
+    path = "/Users/dulguuncodes/.local/share/nvim/site/pack/packer/start/nvim-comment",
+    url = "https://github.com/terrortylor/nvim-comment"
   },
   ["nvim-compe"] = {
-    config = { "\27LJ\2\n¿\4\0\0\a\0\16\1\0216\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0005\3\5\0005\4\4\0=\4\6\0036\4\a\0009\4\b\0046\6\t\0009\6\n\0069\6\v\6\24\6\0\6B\4\2\2=\4\f\3=\3\r\0025\3\14\0=\3\15\2B\0\2\1K\0\1\0\vsource\1\0\b\rnvim_lua\2\rnvim_lsp\2\nvsnip\2\vbuffer\2\tpath\2\fluasnip\2\14ultisnips\2\tcalc\2\18documentation\15max_height\nlines\6o\bvim\nfloor\tmath\vborder\1\0\4\15min_height\3\1\14min_width\3<\14max_width\3x\17winhighlightHNormalFloat:CompeDocumentation,FloatBorder:CompeDocumentationBorder\1\t\0\0\5\5\5\6 \5\5\5\6 \1\0\f\19max_menu_width\3d\19max_kind_width\3d\19max_abbr_width\3d\21incomplete_delay\3\3\20resolve_timeout\3 \6\19source_timeout\3È\1\18throttle_time\3P\fenabled\2\14preselect\venable\15min_length\3\1\17autocomplete\2\ndebug\1\nsetup\ncompe\frequireçÌ™³\6³æÌþ\3\0" },
+    config = { "\27LJ\2\n¿\4\0\0\a\0\16\1\0216\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0005\3\5\0005\4\4\0=\4\6\0036\4\a\0009\4\b\0046\6\t\0009\6\n\0069\6\v\6\24\6\0\6B\4\2\2=\4\f\3=\3\r\0025\3\14\0=\3\15\2B\0\2\1K\0\1\0\vsource\1\0\b\nvsnip\2\vbuffer\2\rnvim_lsp\2\tpath\2\rnvim_lua\2\tcalc\2\fluasnip\2\14ultisnips\2\18documentation\15max_height\nlines\6o\bvim\nfloor\tmath\vborder\1\0\4\14max_width\3x\17winhighlightHNormalFloat:CompeDocumentation,FloatBorder:CompeDocumentationBorder\15min_height\3\1\14min_width\3<\1\t\0\0\5\5\5\6 \5\5\5\6 \1\0\f\ndebug\1\19max_menu_width\3d\19max_kind_width\3d\19max_abbr_width\3d\21incomplete_delay\3\3\20resolve_timeout\3 \6\19source_timeout\3È\1\18throttle_time\3P\14preselect\venable\15min_length\3\1\17autocomplete\2\fenabled\2\nsetup\ncompe\frequireçÌ™³\6³æÌþ\3\0" },
     loaded = true,
-    path = "/Users/dulguuncodes/.local/share/nvim/site/pack/packer/start/nvim-compe"
+    path = "/Users/dulguuncodes/.local/share/nvim/site/pack/packer/start/nvim-compe",
+    url = "https://github.com/hrsh7th/nvim-compe"
   },
   ["nvim-lspconfig"] = {
     loaded = true,
-    path = "/Users/dulguuncodes/.local/share/nvim/site/pack/packer/start/nvim-lspconfig"
+    path = "/Users/dulguuncodes/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
+    url = "https://github.com/neovim/nvim-lspconfig"
+  },
+  ["nvim-luadev"] = {
+    loaded = true,
+    path = "/Users/dulguuncodes/.local/share/nvim/site/pack/packer/start/nvim-luadev",
+    url = "https://github.com/bfredl/nvim-luadev"
+  },
+  ["nvim-notify"] = {
+    loaded = true,
+    path = "/Users/dulguuncodes/.local/share/nvim/site/pack/packer/start/nvim-notify",
+    url = "https://github.com/rcarriga/nvim-notify"
   },
   ["nvim-tree.lua"] = {
-    config = { "\27LJ\2\n;\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\14nvim-tree\frequire\0" },
+    config = { "\27LJ\2\n†\1\0\0\5\0\n\0\r6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\b\0005\3\6\0006\4\3\0009\4\4\0049\4\5\4=\4\a\3=\3\t\2B\0\2\1K\0\1\0\bgit\1\0\0\vignore\1\0\0 peafowl_nvim_tree_gitignore\6g\bvim\nsetup\14nvim-tree\frequire\0" },
     loaded = true,
-    path = "/Users/dulguuncodes/.local/share/nvim/site/pack/packer/start/nvim-tree.lua"
+    path = "/Users/dulguuncodes/.local/share/nvim/site/pack/packer/start/nvim-tree.lua",
+    url = "https://github.com/kyazdani42/nvim-tree.lua"
   },
   ["nvim-treesitter"] = {
     config = { "\27LJ\2\n*\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\15treesitter\frequire\0" },
     loaded = true,
-    path = "/Users/dulguuncodes/.local/share/nvim/site/pack/packer/start/nvim-treesitter"
+    path = "/Users/dulguuncodes/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
+    url = "https://github.com/nvim-treesitter/nvim-treesitter"
   },
   ["nvim-web-devicons"] = {
     loaded = true,
-    path = "/Users/dulguuncodes/.local/share/nvim/site/pack/packer/start/nvim-web-devicons"
+    path = "/Users/dulguuncodes/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
+    url = "https://github.com/kyazdani42/nvim-web-devicons"
   },
   ["packer.nvim"] = {
     loaded = true,
-    path = "/Users/dulguuncodes/.local/share/nvim/site/pack/packer/start/packer.nvim"
+    path = "/Users/dulguuncodes/.local/share/nvim/site/pack/packer/start/packer.nvim",
+    url = "https://github.com/wbthomason/packer.nvim"
   },
   ["plenary.nvim"] = {
     loaded = true,
-    path = "/Users/dulguuncodes/.local/share/nvim/site/pack/packer/start/plenary.nvim"
+    path = "/Users/dulguuncodes/.local/share/nvim/site/pack/packer/start/plenary.nvim",
+    url = "https://github.com/nvim-lua/plenary.nvim"
   },
   ["popup.nvim"] = {
     loaded = true,
-    path = "/Users/dulguuncodes/.local/share/nvim/site/pack/packer/start/popup.nvim"
+    path = "/Users/dulguuncodes/.local/share/nvim/site/pack/packer/start/popup.nvim",
+    url = "https://github.com/nvim-lua/popup.nvim"
   },
   ["telescope.nvim"] = {
     loaded = true,
-    path = "/Users/dulguuncodes/.local/share/nvim/site/pack/packer/start/telescope.nvim"
+    path = "/Users/dulguuncodes/.local/share/nvim/site/pack/packer/start/telescope.nvim",
+    url = "https://github.com/nvim-telescope/telescope.nvim"
+  },
+  ["trouble.nvim"] = {
+    config = { "\27LJ\2\n9\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\ftrouble\frequire\0" },
+    loaded = true,
+    path = "/Users/dulguuncodes/.local/share/nvim/site/pack/packer/start/trouble.nvim",
+    url = "https://github.com/folke/trouble.nvim"
   },
   ["vim-fugitive"] = {
     loaded = true,
-    path = "/Users/dulguuncodes/.local/share/nvim/site/pack/packer/start/vim-fugitive"
+    path = "/Users/dulguuncodes/.local/share/nvim/site/pack/packer/start/vim-fugitive",
+    url = "https://github.com/tpope/vim-fugitive"
   },
   ["vim-hexokinase"] = {
     loaded = true,
-    path = "/Users/dulguuncodes/.local/share/nvim/site/pack/packer/start/vim-hexokinase"
+    path = "/Users/dulguuncodes/.local/share/nvim/site/pack/packer/start/vim-hexokinase",
+    url = "https://github.com/RRethy/vim-hexokinase"
   },
   ["vim-highlightedyank"] = {
     loaded = true,
-    path = "/Users/dulguuncodes/.local/share/nvim/site/pack/packer/start/vim-highlightedyank"
+    path = "/Users/dulguuncodes/.local/share/nvim/site/pack/packer/start/vim-highlightedyank",
+    url = "https://github.com/machakann/vim-highlightedyank"
   },
   ["vim-obsession"] = {
     loaded = true,
-    path = "/Users/dulguuncodes/.local/share/nvim/site/pack/packer/start/vim-obsession"
+    path = "/Users/dulguuncodes/.local/share/nvim/site/pack/packer/start/vim-obsession",
+    url = "https://github.com/tpope/vim-obsession"
   },
   ["vim-windowswap"] = {
     loaded = true,
-    path = "/Users/dulguuncodes/.local/share/nvim/site/pack/packer/start/vim-windowswap"
+    path = "/Users/dulguuncodes/.local/share/nvim/site/pack/packer/start/vim-windowswap",
+    url = "https://github.com/wesQ3/vim-windowswap"
+  },
+  ["zen-mode.nvim"] = {
+    config = { "\27LJ\2\n:\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\rzen-mode\frequire\0" },
+    loaded = true,
+    path = "/Users/dulguuncodes/.local/share/nvim/site/pack/packer/start/zen-mode.nvim",
+    url = "https://github.com/folke/zen-mode.nvim"
   }
 }
 
@@ -183,22 +229,14 @@ time([[Setup for dashboard-nvim]], false)
 time([[packadd for dashboard-nvim]], true)
 vim.cmd [[packadd dashboard-nvim]]
 time([[packadd for dashboard-nvim]], false)
--- Config for: nvim-comment
-time([[Config for nvim-comment]], true)
-try_loadstring("\27LJ\2\n'\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\fcomment\frequire\0", "config", "nvim-comment")
-time([[Config for nvim-comment]], false)
--- Config for: everforest
-time([[Config for everforest]], true)
-try_loadstring("\27LJ\2\nX\0\0\3\0\5\0\b6\0\0\0009\0\1\0'\2\2\0B\0\2\0016\0\3\0'\2\4\0B\0\2\1K\0\1\0\14highlight\frequire\27colorscheme everforest\bcmd\bvim\0", "config", "everforest")
-time([[Config for everforest]], false)
+-- Config for: gitsigns.nvim
+time([[Config for gitsigns.nvim]], true)
+try_loadstring("\27LJ\2\n6\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\rgitsigns\frequire\0", "config", "gitsigns.nvim")
+time([[Config for gitsigns.nvim]], false)
 -- Config for: nvim-tree.lua
 time([[Config for nvim-tree.lua]], true)
-try_loadstring("\27LJ\2\n;\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\14nvim-tree\frequire\0", "config", "nvim-tree.lua")
+try_loadstring("\27LJ\2\n†\1\0\0\5\0\n\0\r6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\b\0005\3\6\0006\4\3\0009\4\4\0049\4\5\4=\4\a\3=\3\t\2B\0\2\1K\0\1\0\bgit\1\0\0\vignore\1\0\0 peafowl_nvim_tree_gitignore\6g\bvim\nsetup\14nvim-tree\frequire\0", "config", "nvim-tree.lua")
 time([[Config for nvim-tree.lua]], false)
--- Config for: nvim-compe
-time([[Config for nvim-compe]], true)
-try_loadstring("\27LJ\2\n¿\4\0\0\a\0\16\1\0216\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0005\3\5\0005\4\4\0=\4\6\0036\4\a\0009\4\b\0046\6\t\0009\6\n\0069\6\v\6\24\6\0\6B\4\2\2=\4\f\3=\3\r\0025\3\14\0=\3\15\2B\0\2\1K\0\1\0\vsource\1\0\b\rnvim_lua\2\rnvim_lsp\2\nvsnip\2\vbuffer\2\tpath\2\fluasnip\2\14ultisnips\2\tcalc\2\18documentation\15max_height\nlines\6o\bvim\nfloor\tmath\vborder\1\0\4\15min_height\3\1\14min_width\3<\14max_width\3x\17winhighlightHNormalFloat:CompeDocumentation,FloatBorder:CompeDocumentationBorder\1\t\0\0\5\5\5\6 \5\5\5\6 \1\0\f\19max_menu_width\3d\19max_kind_width\3d\19max_abbr_width\3d\21incomplete_delay\3\3\20resolve_timeout\3 \6\19source_timeout\3È\1\18throttle_time\3P\fenabled\2\14preselect\venable\15min_length\3\1\17autocomplete\2\ndebug\1\nsetup\ncompe\frequireçÌ™³\6³æÌþ\3\0", "config", "nvim-compe")
-time([[Config for nvim-compe]], false)
 -- Config for: nvim-treesitter
 time([[Config for nvim-treesitter]], true)
 try_loadstring("\27LJ\2\n*\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\15treesitter\frequire\0", "config", "nvim-treesitter")
@@ -207,10 +245,26 @@ time([[Config for nvim-treesitter]], false)
 time([[Config for nvim-cartographer]], true)
 try_loadstring("\27LJ\2\n$\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\tmaps\frequire\0", "config", "nvim-cartographer")
 time([[Config for nvim-cartographer]], false)
--- Config for: gitsigns.nvim
-time([[Config for gitsigns.nvim]], true)
-try_loadstring("\27LJ\2\n6\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\rgitsigns\frequire\0", "config", "gitsigns.nvim")
-time([[Config for gitsigns.nvim]], false)
+-- Config for: zen-mode.nvim
+time([[Config for zen-mode.nvim]], true)
+try_loadstring("\27LJ\2\n:\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\rzen-mode\frequire\0", "config", "zen-mode.nvim")
+time([[Config for zen-mode.nvim]], false)
+-- Config for: nvim-comment
+time([[Config for nvim-comment]], true)
+try_loadstring("\27LJ\2\n'\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\fcomment\frequire\0", "config", "nvim-comment")
+time([[Config for nvim-comment]], false)
+-- Config for: everforest
+time([[Config for everforest]], true)
+try_loadstring("\27LJ\2\nX\0\0\3\0\5\0\b6\0\0\0009\0\1\0'\2\2\0B\0\2\0016\0\3\0'\2\4\0B\0\2\1K\0\1\0\14highlight\frequire\27colorscheme everforest\bcmd\bvim\0", "config", "everforest")
+time([[Config for everforest]], false)
+-- Config for: nvim-compe
+time([[Config for nvim-compe]], true)
+try_loadstring("\27LJ\2\n¿\4\0\0\a\0\16\1\0216\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0005\3\5\0005\4\4\0=\4\6\0036\4\a\0009\4\b\0046\6\t\0009\6\n\0069\6\v\6\24\6\0\6B\4\2\2=\4\f\3=\3\r\0025\3\14\0=\3\15\2B\0\2\1K\0\1\0\vsource\1\0\b\nvsnip\2\vbuffer\2\rnvim_lsp\2\tpath\2\rnvim_lua\2\tcalc\2\fluasnip\2\14ultisnips\2\18documentation\15max_height\nlines\6o\bvim\nfloor\tmath\vborder\1\0\4\14max_width\3x\17winhighlightHNormalFloat:CompeDocumentation,FloatBorder:CompeDocumentationBorder\15min_height\3\1\14min_width\3<\1\t\0\0\5\5\5\6 \5\5\5\6 \1\0\f\ndebug\1\19max_menu_width\3d\19max_kind_width\3d\19max_abbr_width\3d\21incomplete_delay\3\3\20resolve_timeout\3 \6\19source_timeout\3È\1\18throttle_time\3P\14preselect\venable\15min_length\3\1\17autocomplete\2\fenabled\2\nsetup\ncompe\frequireçÌ™³\6³æÌþ\3\0", "config", "nvim-compe")
+time([[Config for nvim-compe]], false)
+-- Config for: trouble.nvim
+time([[Config for trouble.nvim]], true)
+try_loadstring("\27LJ\2\n9\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\ftrouble\frequire\0", "config", "trouble.nvim")
+time([[Config for trouble.nvim]], false)
 if should_profile then save_profiles() end
 
 end)
